6e8c03800104306437c00736f4e90c63a85ac518
Jax Hutton <jax.hutton@outlook.com>
2019-10-28 15:17:24 -0400

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -2,37 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
-void append(list l, int val) {
-    node* n = (node*) malloc(sizeof(node));
-    n->val = val;
-    node *curr = l.head;
-    while (curr->next) {
-        curr = curr->next;
-    }
-    curr->next = n;
-}
-
-list create(int count, ...) {
-    if (count < 1) {
-        return (list) {};
-    }
-    va_list vaList;
-    va_start(vaList, count);
-    list l;
-    node* ptr[count];
-    for (int i = 0; i < count; i++) {
-        node* n = (node*) malloc(sizeof(node));
-        n->val = va_arg(vaList, int);
-        ptr[i] = n;
-        if (i > 0) {
-            ptr[i-1]->next = ptr[i];
-        }
-    }
-    va_end(vaList);
-    l.head = ptr[0];
-    return l;
-}
-
 int size(list l) {
     if (!l.head) {
         return -1;
@@ -44,6 +13,53 @@ int size(list l) {
         c++;
     }
     return c;
+}
+
+node* get_ptr(list l, int index) {
+    node *curr = l.head;
+    for (int i = 0; i < index; i++) {
+        if (!curr->next) {
+            printf("No index found, exiting...");
+            exit(7);
+        }
+        curr = curr->next;
+    }
+    return curr;
+}
+
+void append(list l, int val) {
+    node* n = (node*) malloc(sizeof(node));
+    n->val = val;
+    get_ptr(l, size(l) - 1)->next = n;
+}
+
+void append_node(list l, node* n) {
+    get_ptr(l, size(l) - 1)->next = n;
+}
+
+list create(int count, ...) {
+    if (count < 1) {
+        return (list) {};
+    }
+    va_list vaList;
+    va_start(vaList, count);
+    node* n = (node*) malloc(sizeof(node));
+    n->val = va_arg(vaList, int);
+    list l = {.head = n};
+    for (int i = 0; i < count - 1; i++) {
+        append(l, va_arg(vaList, int));
+    }
+    va_end(vaList);
+    return l;
+}
+
+list create_with_node(node* n) {
+    list l = {.head = n};
+    return l;
+}
+
+int get(list l, int index) {
+    return get_ptr(l, index)->val;
 }
 
 node* search(list l, int val) {
@@ -76,41 +92,36 @@ int index_search(list l, int val) {
     return -1;
 }
 
-list index_search_all(list l, int val) {
+list search_all(list l, int val) {
     node* curr = l.head;
     list indices = {};
-    int c = 0;
-    while(curr->next) {
+    for (int i = 0; i < size(l); i++) {
         if (curr->val == val) {
             if (!indices.head) {
-                indices = create(1, c);
+                indices = create_with_node(curr);
             }
             else {
-                append(indices, c);
+                append_node(indices, curr);
             }
         }
-        c++;
         curr = curr->next;
-    }
-    if (curr->val == val) {
-        if (!indices.head) {
-            indices = create(1, c);
-        }
-        else {
-            append(indices, c);
-        }
     }
     return indices;
 }
 
-int get(list l, int index) {
-    node *curr = l.head;
-    for (int i = 0; i < index; i++) {
-        if (!curr->next) {
-            printf("No index found, exiting...");
-            exit(7);
+list index_search_all(list l, int val) {
+    node* curr = l.head;
+    list indices = {};
+    for (int i = 0; i < size(l); i++) {
+        if (curr->val == val) {
+            if (!indices.head) {
+                indices = create(1, i);
+            }
+            else {
+                append(indices, i);
+            }
         }
         curr = curr->next;
     }
-    return curr->val;
+    return indices;
 }
